0d85aac4bc9099eff8371fa2076a81ff64ac43e5
nong10 <nong10w@outlook.com>
2020-10-19 15:11:54 +0200

    update config.h

--- config.h
+++ config.h
@@ -82,10 +82,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	/* rotate stack */
-//	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
-//	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	/* rotate stack ends */
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
