ec191d22d8c9fa7609340a41b323fda9e40e0113
nong10 <nong10w@outlook.com>
2020-10-27 19:25:52 +0100

    map open dmenu into MOD + r

--- config.h
+++ config.h
@@ -80,7 +80,7 @@ static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	/* rotate stack */
