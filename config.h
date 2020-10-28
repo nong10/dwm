d4387986fa3d254494dc91fe915bc7d7a898994c
nong10 <nong10w@outlook.com>
2020-10-28 16:55:07 +0100

    update color of cyan

--- config.h
+++ config.h
@@ -1,5 +1,3 @@
-/* See LICENSE file for copyright and license details. */
-
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
@@ -17,7 +15,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#3585C6";
+static const char col_cyan[]        = "#005577";
 static const char col_border[]        = "#3585C6";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
