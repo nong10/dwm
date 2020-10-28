043390e458d05b2503adbc302deab8bc476258c4
nong10 <nong10w@outlook.com>
2020-10-28 17:10:16 +0100

    update color

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=15" };
 static const char dmenufont[]       = "monospace:size=15";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#00384f";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
