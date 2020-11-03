4fcbd23da14a827aeb14c7006c2afcce8986f67f
nong10 <nong10w@outlook.com>
2020-11-03 16:57:33 +0100

    modified border pixel

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 6;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 6;       /* vert inner gap between windows */
